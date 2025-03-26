a5aa6f377b85b195629e24a57c3efeedc9057fcd
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2025-03-26 14:49:53 +0200

    Ex. 9.1 bmiCalculator

---
+++ part-a/bmiCalculator.ts
@@ -0,0 +1,25 @@
+
+const calculateBmi = (heightCm: number, weightKg: number): string => {
+  const heightInMeters = heightCm / 100;
+  const bmi = weightKg / (heightInMeters * heightInMeters);
+
+  if (bmi < 16) {
+    return 'Underweight (Severe thinness)';
+  } else if (bmi < 17) {
+    return 'Underweight (Moderate thinness)';
+  } else if (bmi < 18.5) {
+    return 'Underweight (Mild thinness)';
+  } else if (bmi < 25) {
+    return 'Normal range';
+  } else if (bmi < 30) {
+    return 'Overweight (Pre-obese)';
+  } else if (bmi < 35) {
+    return 'Obese (Class I)';
+  } else if (bmi < 40) {
+    return 'Obese (Class II)';
+  } else {
+    return 'Obese (Class III)';
+  }
+};
+
+console.log(calculateBmi(180, 74));
